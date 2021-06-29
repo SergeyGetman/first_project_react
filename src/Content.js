93e8583335ae793f847a4451ef9b4633e17f182b
Sergey <biznesgroup2017@gmail.com>
2021-06-29 20:10:29 +0300

    split the language into components

---
+++ src/Content.js
@@ -0,0 +1,14 @@
+
+
+const Content = () => {
+    return (
+    <h1>This is Apple </h1>,
+
+    <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+        <a> Aperiam commodi</a>  fugit laborum magnam maxime nulla quas,
+        quidem quo repellendus rerum totam vel veniam.
+        Atque aut blanditiis eaque impedit ipsam odit!</p>
+
+    )
+}
+export default Content;
