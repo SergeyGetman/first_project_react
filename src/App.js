a81bb1b7bdb76e74df9bb58570caa14cbd2445dc
Sergey <biznesgroup2017@gmail.com>
2021-07-01 19:46:05 +0300

    encapsulation modules

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
 import React from "react";
 import './App.css';
-import Header from "./components/Header";
-import Navbar from "./components/Navbar";
-import Profile from "./components/Profile";
+import Header from "./components/Header/Header";
+import Navbar from "./components/Navbar/Navbar";
+import Profile from "./components/Profile/Profile";
 
 
 
-const App = () =>  {
+
+
+
+
+const App = () => {
   return (
-      <div className="app-wrapper">
-                <Header/>
-                <Navbar/>
-                <Profile/>
+    <div className="app-wrapper">
+      <Header/>
+      <Navbar/>
+      <Profile/>
 
-              </div>
+
+
+    </div>
   );
+
 }
 export default App;
--- src/Content.js
+++ src/Content.js
@@ -1,14 +1,18 @@
 
 
+
+
 const Content = () => {
-    return (
+
+  return (
+
     <h1>This is Apple </h1>,
 
-    <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+      <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit.
         <a> Aperiam commodi</a>  fugit laborum magnam maxime nulla quas,
         quidem quo repellendus rerum totam vel veniam.
         Atque aut blanditiis eaque impedit ipsam odit!</p>
 
-    )
+  )
 }
 export default Content;
