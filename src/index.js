8721d2d86bf03bd84caa3b4d841e72f64263993a
Sergey <biznesgroup2017@gmail.com>
2021-08-31 19:58:38 +0300

    use store object

--- src/index.js
+++ src/index.js
@@ -1,21 +1,22 @@
 import App from './App';
 import React from 'react';
-import state from "./redux/state";
+import store from "./redux/state";
 import ReactDOM from 'react-dom';
 import './index.css';
-import {addPost, subscribe, updateNewPostText} from "./redux/state"
+
 
 
 let rerenderEntireTree = (state) => {
   ReactDOM.render(
     <React.StrictMode>
-      <App state={state} addPost={addPost} updateNewPostText={updateNewPostText} />
+      <App state={store.getState()} addPost={store.addPost}
+           updateNewPostText={store.updateNewPostText} />
     </React.StrictMode>,
     document.getElementById('root')
   );
 };
 
-rerenderEntireTree(state)
-subscribe(rerenderEntireTree);
+rerenderEntireTree(store.getState())
+store.subscribe(rerenderEntireTree);
 
 
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,71 +1,72 @@
-let rerenderEntireTree = () => {
-  console.log("State is changed")
-}
+let store = {
+ _state : {
 
-let state = {
+   text : {
+     id : 1,
+     messageInside : "Ты думаешь ты самый умный чел на земле ? LOL"
+   },
 
-  text : {
-    id : 1,
-    messageInside : "Ты думаешь ты самый умный чел на земле ? LOL"
+   profilePage : {
+     postData : [
+       {id: 1, message: "How are you ?", likesCount: 'like 23'},
+       {id: 2, message: "It'/s my first post", likesCount: 'like 2'},
+       {id: 3, message: "wow its work", likesCount: 'like 13'},
+
+     ],
+     newPostText : "My favorite",
+     sidebar : {
+       mess : "asdqweqwe sad qw eq weq e"
+     }, // внести сюда картинки для лого
+
+   },
+
+   dialogsPage : {
+     messagesData : [
+       {id: 1, message: "How are you ?"},
+       {id: 2, message: "It'"},
+       {id: 3, message: "wow"},
+       {id: 4, message: "wow"},
+       {id: 5, message: "wow"},
+
+     ],
+     dialogsData : [
+       {id: 1, name: "Sergodjan"},
+       {id: 2, name: "Alex"},
+       {id: 3, name: "Martin"},
+       {id: 4, name: "Keer"},
+       {id: 5, name: "Locky"},
+     ],
+   }
+
+ },
+  getState(){
+   return _state
   },
 
-  profilePage : {
-    postData : [
-      {id: 1, message: "How are you ?", likesCount: 'like 23'},
-      {id: 2, message: "It'/s my first post", likesCount: 'like 2'},
-      {id: 3, message: "wow its work", likesCount: 'like 13'},
-
-    ],
-    newPostText : "My favorite",
-    sidebar : {
-     mess : "asdqweqwe sad qw eq weq e"
-    }, // внести сюда картинки для лого
-
+  rerenderEntireTree ()  {
+    console.log("State is changed")
   },
+  addPost () {
 
- dialogsPage : {
-    messagesData : [
-      {id: 1, message: "How are you ?"},
-      {id: 2, message: "It'"},
-      {id: 3, message: "wow"},
-      {id: 4, message: "wow"},
-      {id: 5, message: "wow"},
-
-    ],
-    dialogsData : [
-      {id: 1, name: "Sergodjan"},
-      {id: 2, name: "Alex"},
-      {id: 3, name: "Martin"},
-      {id: 4, name: "Keer"},
-      {id: 5, name: "Locky"},
-    ],
+    let newPost = {
+      id : 5,
+      message : state.profilePage.newPostText ,
+      likesCount : 0
+    };
+    state.profilePage.postData.push(newPost);
+    state.profilePage.newPostText = "";
+    rerenderEntireTree(state)
+  },
+  updateNewPostText (newTetx)  {
+    state.profilePage.newPostText = newTetx;
+    rerenderEntireTree(state)
+  },
+  subscribe (observer){
+    rerenderEntireTree = observer
   }
-
 }
 
-window.state = state;
+window.store = store;
+export default store;
 
-export let addPost;
 
-addPost = () => {
-
-  let newPost = {
-     id : 5,
-     message : state.profilePage.newPostText ,
-     likesCount : 0
-  };
-  state.profilePage.postData.push(newPost);
-  state.profilePage.newPostText = "";
-  rerenderEntireTree(state)
-}
-
-export const updateNewPostText = (newTetx) => {
-  state.profilePage.newPostText = newTetx;
-  rerenderEntireTree(state)
-}
-
-export const subscribe = (observer) =>  {
-rerenderEntireTree = observer
-}
-
-export default state;
